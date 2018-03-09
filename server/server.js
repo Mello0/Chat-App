23c19782a0c4278da159133d7309840fa7737932
Mello <lasri.mouad98@gmail.com>
2018-03-09 03:23:46 +0000

    Init commit

---
+++ server/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+const publicPath = path.join(__dirname + '/public'); // static folder path
+const port = process.env.PORT || 3000;  // for heroku
+
+console.log(publicPath);
+app.use(express.static(publicPath));
+
+app.listen(port, () => {
+    console.log('Listening to port ' + port);
+});
