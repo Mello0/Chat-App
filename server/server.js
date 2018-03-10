337b4f0eb2d9c5c3a54c91c2e057865c4e83a177
Mello <lasri.mouad98@gmail.com>
2018-03-10 17:50:47 +0000

    Greet new user, and alert others

--- server/server.js
+++ server/server.js
@@ -18,13 +18,33 @@ io.on('connection', (socket) => {
     // socket.emit : sending msg to client (sending msg)
     // socket.on : receiving msg from client (listening to inc. msgs)
 
+    // Welcome message from admin
+    socket.emit('newMessage', {
+       from: 'Admin',
+       text: 'Welcome to the chat',
+       createdAt: new Date().getTime()
+    });
+
+    // User joined message broadcasted
+    socket.broadcast.emit('newMessage', {
+        from: 'Admin',
+        text: 'New user joined',
+        createdAt: new Date().getTime()
+    });
+
     socket.on('createMessage', (message) => {
         console.log('Create Message (received from client): ', message);
+    
         io.emit('newMessage', { // emit msg to all sockets
             from: message.from,
             text: message.text,
             createdAt: new Date().getTime()
         }); 
+        // socket.broadcast.emit('newMessage', {
+        //     from: 'Mello',
+        //     text: 'Hello there!',
+        //     createdAt: new Date().getTime()
+        // });
     });
 
     socket.on('disconnect', () => {
