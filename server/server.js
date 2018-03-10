44c9a991c6f6d48a698a812b83f85ae72cc9bb8c
Mello <lasri.mouad98@gmail.com>
2018-03-10 15:52:57 +0000

    File paths

--- server/server.js
+++ server/server.js
@@ -2,12 +2,12 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-const publicPath = path.join(__dirname + '/public'); // static folder path
+const publicPath = path.join(__dirname + '/../public'); // static folder path
 const port = process.env.PORT || 3000;  // for heroku
 
 console.log(publicPath);
 app.use(express.static(publicPath));
-
+ 
 app.get('/', (req, res) => {
     res.sendFile('index.html');
 });
